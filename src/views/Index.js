b6418d73e7f88a39959df0327ee0c2abb97bc5d7
appflexion <mujeeb.uok@gmail.com>
2021-03-30 18:14:02 +0500

    cleaning UI components files

--- src/views/Index.js
+++ src/views/Index.js
@@ -1,33 +1,14 @@
-/*!
 
-=========================================================
-* Argon Dashboard React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React, { useState } from "react";
 // node.js library that concatenates classes (strings)
 import classnames from "classnames";
-// javascipt plugin for creating charts
-import Chart from "chart.js";
-// react plugin used to create charts
-import { Line, Bar } from "react-chartjs-2";
 // reactstrap components
 import {
   Button,
   Card,
   CardHeader,
   CardBody,
+  CardTitle,
   NavItem,
   NavLink,
   Nav,
@@ -38,35 +19,16 @@ import {
   Col,
 } from "reactstrap";
 
-// core components
-import {
-  chartOptions,
-  parseOptions,
-  chartExample1,
-  chartExample2,
-} from "variables/charts.js";
-
 import Header from "components/Headers/Header.js";
 
 const Index = (props) => {
   const [activeNav, setActiveNav] = useState(1);
-  const [chartExample1Data, setChartExample1Data] = useState("data1");
-
-  if (window.Chart) {
-    parseOptions(Chart, chartOptions());
-  }
-
-  const toggleNavs = (e, index) => {
-    e.preventDefault();
-    setActiveNav(index);
-    setChartExample1Data("data" + index);
-  };
   return (
     <>
       <Header />
       {/* Page content */}
       <Container className="mt--7" fluid>
-       <Row className="mt-5">
+        <Row className="mt-5">
           <Col className="mb-5 mb-xl-0" xl="12">
             <Card className="shadow">
               <CardHeader className="border-0">
@@ -74,16 +36,16 @@ const Index = (props) => {
                   <div className="col">
                     <h3 className="mb-0">Projects</h3>
                   </div>
-                  <div className="col text-right">
-                   
-                  </div>
+                  <div className="col text-right"></div>
+                  <Container>
+                    <h3>Hi demo content here</h3>
+                  </Container>
                 </Row>
               </CardHeader>
-            {/* content for projects card starts here */}
-            <h3>Your content</h3>
+           
             </Card>
           </Col>
-        </Row>
+        </Row> 
       </Container>
     </>
   );
